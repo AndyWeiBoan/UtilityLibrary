604a523b274011d6540f9b16983327c8b3faadc2
ANDY WEI <andyweiboan@gmail.com>
2019-08-18 22:57:33 +0800

    init

--- UtilityLibaray/Test/Integration/IniUtilTest.cs
+++ UtilityLibaray/Test/Integration/IniUtilTest.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using NUnit.Framework;
 
 
@@ -15,6 +11,7 @@ namespace UtilityLibaray.Test.Integration {
 
         private string _file =Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Test\Integration\Setting.ini");
 
+        
         [TestCase("Unicode", "Unicode", "")]
         [TestCase("System Access", "NewAdministratorName", "Administrator")]
         [TestCase("System Access", "NewGuestName", "Guest")]
@@ -28,13 +25,28 @@ namespace UtilityLibaray.Test.Integration {
         }
 
         
-        [TestCase("Event Audit", "AuditSystemEvents", "9")]
+        [TestCase("Event Audit", "AuditSystemEvents", "10")]
         public void INI檔案不存在(string section, string key, string expected) {
 
             var NotExistsFile = @"%Windir%/123.ini";
 
             Assert.Catch<FileNotFoundException>(()=> IniUtil.Read(NotExistsFile, section, key));
 
+        }
+
+        [TestCase("Event Audit", "AuditSystemEvents", "11")]
+        public void INI檔案寫入(string section, string key, string expected) {
+
+            var before = IniUtil.Read(this._file, section, key);
+
+            IniUtil.Write(this._file, section, key, expected);
+
+            var actual = IniUtil.Read(this._file, section, key);
+
+            Assert.AreEqual(expected, actual);
+
+            // snapshot
+            IniUtil.Write(this._file, section, key, before);
         }
     }
 }
