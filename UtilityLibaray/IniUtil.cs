fb62ce6ff97407bbc6a8fa437863e892026e7802
ANDY WEI <andyweiboan@gmail.com>
2019-08-18 22:50:54 +0800

    加入專案檔案。

---
+++ UtilityLibaray/IniUtil.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace UtilityLibaray {
+
+    public static class IniUtil {
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(
+            string section,
+            string key,
+            string val,
+            string file);
+
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(
+            string section,
+            string key,
+            string def,
+            StringBuilder retVal,
+            int size,
+            string file);
+
+        private static bool EnsureFileExists(string file) {
+            if (!File.Exists(file))
+                throw new FileNotFoundException(file);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Write a value to a specific section with a particular key in ini file,
+        /// and it will returns a integer value. (success: great than 0; failed: Equals 0)
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static int Write(string file,string section,string key,string value) {
+
+            EnsureFileExists(file);
+
+            return (int)WritePrivateProfileString(section, key, value, file);
+        }
+
+        /// <summary>
+        /// Read a specific section with a particular key from a ini file and returns a string value.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Read(string file, string section, string key) {
+
+            EnsureFileExists(file);
+
+            var retVal = new StringBuilder(256);
+            GetPrivateProfileString(section, key, "", retVal, 256, file);
+            return retVal.ToString();
+        }
+
+    }
+}
---
+++ UtilityLibaray/Test/Integration/IniUtilTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace UtilityLibaray.Test.Integration {
+
+    [TestFixture]
+    public class IniUtilTest {
+
+
+        private string _file =Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Test\Integration\Setting.ini");
+
+        [TestCase("Unicode", "Unicode", "")]
+        [TestCase("System Access", "NewAdministratorName", "Administrator")]
+        [TestCase("System Access", "NewGuestName", "Guest")]
+        [TestCase("Event Audit", "AuditSystemEvents", "9")]
+        public void 取得INI檔案設定值(string section, string key,string expected) {
+
+            var actual = IniUtil.Read(this._file, section, key);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        
+        [TestCase("Event Audit", "AuditSystemEvents", "9")]
+        public void INI檔案不存在(string section, string key, string expected) {
+
+            var NotExistsFile = @"%Windir%/123.ini";
+
+            Assert.Catch<FileNotFoundException>(()=> IniUtil.Read(NotExistsFile, section, key));
+
+        }
+    }
+}
